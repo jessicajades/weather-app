3ecca87ef32594c17a49235ae8b29d5455c13e59
Jessica Shepherd <jessicajadecodes@gmail.com>
2020-03-20 14:59:44 -0700

    added styles

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 import React from "react";
+import icons from "./icons/a01d.png";
 
 const WeatherItem = ({ weather }) => {
+    console.log("weather", icons);
+
     return (
         <div className="item-container">
             <div className="item-data">
+                {weather.valid_date}
+                <img src="../icons/a01d.png" className="weather-icon" />
+                <br />
                 Low: {weather.min_temp} / High: {weather.max_temp}
             </div>
         </div>
